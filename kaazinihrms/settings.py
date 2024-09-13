f4a6d00c72fdd85962be600a9318ba2e7511cd7b
goorani <godfreydrani626@gmail.com>
2024-09-14 02:06:09 +0300

    add allowed hosts

--- kaazinihrms/settings.py
+++ kaazinihrms/settings.py
@@ -39,7 +39,8 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['palmcorps.kaazini.com', 'kaazini.com', 'localhost']
 
 
 
