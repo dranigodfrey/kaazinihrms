62458560681dc03dd5b145c00f3dffa93254c55a
goorani <godfreydrani626@gmail.com>
2024-09-14 00:08:34 +0300

    remove database port

--- kaazinihrms/settings.py
+++ kaazinihrms/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_gqvf6e46_eycpcct9btot&4(!86g*4+u1-fu79jjo%zyzc#%9'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -153,7 +153,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
