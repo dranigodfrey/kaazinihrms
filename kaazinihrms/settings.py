2b6b6f8b34ede5374f8163e16e71ca2cd20187a0
goorani <godfreydrani626@gmail.com>
2024-09-20 17:06:56 +0300

    add csrf trusted origin

--- kaazinihrms/settings.py
+++ kaazinihrms/settings.py
@@ -226,3 +226,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'leave_dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+
+CSRF_TRUSTED_ORIGINS = ['https://kaazini.com']
